78ecdc9748959b1bcf2b702755d693e6dacfb893
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-08-11 09:51:40 +0800

    passed test case1: 2S and AD

--- src/main/java/com/thoughtworks/tdd/CardGame.java
+++ src/main/java/com/thoughtworks/tdd/CardGame.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.tdd;
 
 public class CardGame {
+
+    private  static  String []cardNumber=new String[]{"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+    public int getIndex(String number){
+        for(int i=0;i<cardNumber.length;i++){
+            if(cardNumber[i].equals(number)){
+                return i;
+            }
+        }
+        return  -1;
+    }
     public String compareCard(Card card1, Card card2) {
-        return null;
+        String result1="";
+        if(getIndex(card1.getNumber())>getIndex(card2.getNumber())){
+            return result1+= "card1 won";
+        }else{
+            result1+="card2 won";
+        }
+        return result1;
     }
 }
